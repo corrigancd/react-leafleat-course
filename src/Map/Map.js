f9b8e3615597f03610702bcbd75e4de008438a2e
edwin corrigan <edwintcorrigan@gmail.com>
2022-01-18 22:02:54 +0000

    7 Adding MarkerLayer and rendering cities

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,22 +1,20 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
-import { defaultIcon } from "../icons/defaultIcon";
+import { MarkerLayer } from "../layers/MarkerLayer";
+
+import { cities } from "../data/cities";
 
 export const Map = () => {
-  const position = [51.505, -0.09];
+  const position = [0, 0];
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={position} zoom={2} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position} icon={defaultIcon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <MarkerLayer data={cities} />
     </MapContainer>
   );
 };
---
+++ src/layers/MarkerLayer.js
@@ -0,0 +1,22 @@
+import { Marker, Popup, useMap } from "react-leaflet";
+
+import { defaultIcon } from "../icons/defaultIcon";
+
+export const MarkerLayer = ({ data }) => {
+
+  return data.features.map((feature) => {
+    const { coordinates } = feature.geometry;
+
+    return (
+      <Marker
+        key={String(coordinates)}
+        position={[coordinates[1], coordinates[0]]}
+        icon={defaultIcon}
+      >
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    );
+  });
+};
