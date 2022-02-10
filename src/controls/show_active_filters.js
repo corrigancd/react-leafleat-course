0bf9717740657f1b0e037f437485ab5301eb4cec
edwin corrigan <edwintcorrigan@gmail.com>
2022-02-10 17:49:55 +0000

    37 building out UI for show filters control

--- src/controls/show_active_filters.js
+++ src/controls/show_active_filters.js
@@ -1,12 +1,25 @@
 import ReactDOM, { unmountComponentAtNode } from "react-dom";
 import { createControlComponent } from "@react-leaflet/core";
 import { Control, DomUtil } from "leaflet";
+import { List } from "antd";
 
 const node = DomUtil.create("div");
 
 const renderActiveFilters = () => {
   ReactDOM.render(
-    <div>Hello</div>,
+    <div className={"leaflet-control-layers"}>
+      <List
+        size="small"
+        header={
+          <div>
+            <b>Active Filters</b>
+          </div>
+        }
+        bordered
+        dataSource={[]}
+        renderItem={(item) => <List.Item>{item}</List.Item>}
+      />
+    </div>,
     node
   );
 };
