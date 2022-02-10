8639128f7f3be0d3052d3ad0981ebda359226f51
edwin corrigan <edwintcorrigan@gmail.com>
2022-02-10 18:28:08 +0000

    39 - rendering active filters in control

--- src/controls/show_active_filters.js
+++ src/controls/show_active_filters.js
@@ -5,8 +5,29 @@ import { List } from "antd";
 
 const node = DomUtil.create("div");
 
-const renderActiveFilters = (filters = {}) => {
-  console.log(filters);
+const renderActiveFilters = ({ geoFilter, radiusFilter } = {}) => {
+  const getDisplayFilters = () => {
+    const filtersToDisplay = [];
+
+    const round = (num) => Math.round(num * 100) / 100;
+
+    if (geoFilter) {
+      filtersToDisplay.push(geoFilter.properties.CONTINENT);
+    }
+
+    if (radiusFilter) {
+      const { coordinates } = radiusFilter.feature.geometry;
+      const { radius } = radiusFilter;
+      const radiusFilterToDisplay = `
+      Center: (Lat: ${round(coordinates[1])}, Lon: ${round(coordinates[0])}) 
+      Radius: ${radius} km`;
+      filtersToDisplay.push(radiusFilterToDisplay);
+    }
+
+    return filtersToDisplay.length > 0
+      ? filtersToDisplay
+      : ["No Filter Active"];
+  };
 
   ReactDOM.render(
     <div className={"leaflet-control-layers"}>
@@ -18,7 +39,7 @@ const renderActiveFilters = (filters = {}) => {
           </div>
         }
         bordered
-        dataSource={[]}
+        dataSource={getDisplayFilters()}
         renderItem={(item) => <List.Item>{item}</List.Item>}
       />
     </div>,
