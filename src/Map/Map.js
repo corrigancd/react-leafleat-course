cc4da1d2033eb55a2ac88a9ab3b5b90e64b9e8ac
edwin corrigan <edwintcorrigan@gmail.com>
2022-03-06 23:03:26 +0000

    9a rendering cities layer as functional component

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -3,20 +3,37 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import { defaultIcon } from "../icons/defaultIcon";
 
-export const Map = () => {
-  const position = [51.505, -0.09];
+import { cities } from "../data/cities";
 
-  return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position} icon={defaultIcon}>
+ const MarkerLayer = ({ data }) => {
+
+  return data.features.map((feature) => {
+    const { coordinates } = feature.geometry;
+
+    return (
+      <Marker
+        key={String(coordinates)}
+        position={[coordinates[1], coordinates[0]]}
+        icon={defaultIcon}
+      >
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+    );
+  });
+};
+
+export const Map = () => {
+  const position = [0, 0];
+
+  return (
+    <MapContainer center={position} zoom={1} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MarkerLayer data={cities} />
     </MapContainer>
   );
 };
