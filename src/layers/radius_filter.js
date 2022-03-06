908aa5b908bd6a9595a8fd597e023742bff8d2f6
edwin corrigan <edwintcorrigan@gmail.com>
2022-03-06 23:42:27 +0000

    21 Styling radius filter circle

--- src/layers/radius_filter.js
+++ src/layers/radius_filter.js
@@ -13,6 +13,9 @@ export const RadiusFilter = ({ radiusFilter, setRadiusFilter }) => {
             setRadiusFilter(null);
           },
         }}
+        color={"gray"}
+        weight={1}
+        fillOpacity={0.4}
       />
     );
   } else {
