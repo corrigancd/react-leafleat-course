06a6e877643cda708782e96e68dfd983a8e607ca
edwin corrigan <edwintcorrigan@gmail.com>
2022-03-06 22:58:41 +0000

    7 Initialising first react leaflet marker

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import { defaultIcon } from "../icons/defaultIcon";
 
 export const Map = () => {
   const position = [51.505, -0.09];
@@ -10,6 +12,11 @@ export const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={position} icon={defaultIcon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 };
