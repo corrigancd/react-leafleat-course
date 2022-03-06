d30b802f61ffebed413474f17e3591c23b4b0a22
edwin corrigan <edwintcorrigan@gmail.com>
2022-03-06 23:11:11 +0000

    10 Rendering marker layer with tooltip

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -2,8 +2,10 @@ import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 
 import { cities } from "../data/cities";
+import { mountains } from "../data/highest_points";
 
 import { MarkerLayer } from "../layers/marker_layer";
+import { MarkerLayerWithTooltip } from "../layers/marker_layer_with_tooltip";
 
 export const Map = () => {
   const position = [0, 0];
@@ -15,6 +17,7 @@ export const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <MarkerLayer data={cities} />
+      <MarkerLayerWithTooltip data={mountains} />
     </MapContainer>
   );
 };
---
+++ src/layers/marker_layer_with_tooltip.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Marker, Tooltip, useMap } from "react-leaflet";
+import { defaultIcon } from "../icons/defaultIcon";
+
+export const MarkerLayerWithTooltip = ({ data }) => {
+  const leafletMap = useMap();
+  return data.features.map((feature) => {
+    const { coordinates } = feature.geometry;
+    const { name, elevation, continent } = feature.properties;
+    return (
+      <Marker
+        key={String(coordinates)}
+        position={[coordinates[1], coordinates[0]]}
+        icon={defaultIcon}
+      >
+        <Tooltip>
+          <h3>Mt. {name}</h3>
+          Continent: <b>{continent}</b> <br />
+          Elevation: <b>{elevation} m</b>
+        </Tooltip>
+      </Marker>
+    );
+  });
+};
